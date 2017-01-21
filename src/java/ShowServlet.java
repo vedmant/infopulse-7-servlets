981d1df132268d47874aa53a5718b6219a7f01bd
Vedmant <vedmant@gmail.com>
2017-01-21 16:43:40 +0200

    Cookies, sessions example

---
+++ src/java/ShowServlet.java
@@ -0,0 +1,13 @@
+/**
+ * Created by vedmant on 1/21/17.
+ */
+@javax.servlet.annotation.WebServlet(name = "ShowServlet")
+public class ShowServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+}
